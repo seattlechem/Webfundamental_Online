bf23c15b0713cfa42162c6205e07b7eda320983b
seattlechem <seattlechem@yahoo.com>
2017-07-20 12:51:46 -0700

    Adding Birthday counting java excercise

---
+++ JavaScript/WhileYouWait/WhileYouWait.js
@@ -0,0 +1,16 @@
+var daysUntilMyBirthday = 60;
+while (daysUntilMyBirthday>=30) {
+	console.log(daysUntilMyBirthday + " days until my birthday. Such a long time... :(");
+	daysUntilMyBirthday--;
+}
+while (daysUntilMyBirthday >=5){
+	console.log(daysUntilMyBirthday + " days until my brithday!!");
+	daysUntilMyBirthday--;	
+}
+while (daysUntilMyBirthday >0){
+	console.log(daysUntilMyBirthday + " days until my birthday $%#@#$@%#@ ........$%#@#$@%#@");
+	daysUntilMyBirthday--;
+}
+if(daysUntilMyBirthday == 0){
+	console.log("Happy Birthday");
+}
